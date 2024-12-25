28e3ee3e1b2a0d9509fa1207f456c19079484157
umermine <umermine4@gmail.com>
2024-12-25 17:38:01 +0500

    homework 7

---
+++ activity1.py
@@ -0,0 +1,7 @@
+#Adding variables
+age = int(input("Enter your age "))
+#Writing code
+if age < 20 and age > 10:
+    print("You are Allowed and between 10 to 20")
+else:
+    print("You are not Allowed and older or younger than 10 to 20")
